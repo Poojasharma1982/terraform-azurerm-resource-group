5e6e56996a86009361de693dc61c4bba46de2cc8
Poojasharma1982 <89822894+Poojasharma1982@users.noreply.github.com>
2021-09-27 23:04:52 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "azure-rg" {
   name     = var.rg-name
-  location = var.rg-location
+  location = "eastus2"
   tags = {
       Environment = "Test"
       Service     = "Example"
