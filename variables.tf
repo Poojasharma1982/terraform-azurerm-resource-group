371e59b131db83c7e07a55bc9e84cb04b6c7be9d
Poojasharma1982 <89822894+Poojasharma1982@users.noreply.github.com>
2021-09-27 22:38:18 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "rg-name" {
+  type = string
+  #default = "01000000-0000-4000-8000-000030080200"
+}
+variable "rg-location" {
+  type = string
+  #default = "01000000-0000-4000-8000-000030080200"
+}
