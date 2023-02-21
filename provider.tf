ee63870dc57c1c5989f4d180ad4a5f4a9035a87e
Poojasharma1982 <89822894+Poojasharma1982@users.noreply.github.com>
2023-02-21 18:19:36 -0500

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.33.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+
+  subscription_id = "f529b8de-6bf5-4f25-8139-8b71a2feca19"
+
+  client_id = "1ac3bf5e-d72d-4f82-b3b2-b38166413443"
+  tenant_id = "6945611d-3a2c-40de-8677-b27a48587e06"
+  client_secret = "N~I8Q~DKBS5Ya0jUSOpcvcjDIgrUYBK4UKFAebTp"
+}
