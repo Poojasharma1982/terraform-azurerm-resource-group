4582c10d4b6dcaf99e434e7d6d23bd07cdb6a39d
Poojasharma1982 <89822894+Poojasharma1982@users.noreply.github.com>
2023-02-22 15:10:00 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,12 +8,4 @@ terraform {
 }
 
 # Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
 
-  subscription_id = "f529b8de-6bf5-4f25-8139-8b71a2feca19"
-
-  client_id = "1ac3bf5e-d72d-4f82-b3b2-b38166413443"
-  tenant_id = "6945611d-3a2c-40de-8677-b27a48587e06"
-  client_secret = "N~I8Q~DKBS5Ya0jUSOpcvcjDIgrUYBK4UKFAebTp"
-}
